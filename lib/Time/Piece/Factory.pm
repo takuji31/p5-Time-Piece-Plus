933c9d5e2673394fa87fe02069b52bc17259c791
Nishibayashi Takuji <takuji@senchan.jp>
2011-11-21 16:45:48 +0900

    initial commit

---
+++ lib/Time/Piece/Factory.pm
@@ -0,0 +1,32 @@
+package Time::Piece::Factory;
+use strict;
+use warnings;
+our $VERSION = '0.01';
+
+1;
+__END__
+
+=head1 NAME
+
+Time::Piece::Factory -
+
+=head1 SYNOPSIS
+
+  use Time::Piece::Factory;
+
+=head1 DESCRIPTION
+
+Time::Piece::Factory is
+
+=head1 AUTHOR
+
+Nishibayashi Takuji E<lt>takuji {at} senchan.jpE<gt>
+
+=head1 SEE ALSO
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
