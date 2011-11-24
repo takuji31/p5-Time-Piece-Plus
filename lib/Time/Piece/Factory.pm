ede9939e7367a3d3695954a6c146c106cb4ea501
Nishibayashi Takuji <takuji@senchan.jp>
2011-11-24 15:44:15 +0900

    add yesterday tommorow

--- lib/Time/Piece/Factory.pm
+++ lib/Time/Piece/Factory.pm
@@ -1,18 +1,73 @@
 package Time::Piece::Factory;
 use strict;
 use warnings;
+use 5.10.0;
 our $VERSION = '0.01';
+
+use Time::Piece;
+
+sub import {
+    my $class  = shift;
+    my $caller = caller;
+
+    unshift @_, 'Time::Piece';
+
+    goto &Time::Piece::import;
+}
+
+package  Time::Piece;
+use strict;
+use warnings;
+
+use Time::Seconds;
+use Data::Validator;
+
+sub get_object {
+    my ($self, ) = @_;
+
+    $self = $self->localtime unless ref $self;
+    return $self;
+}
+
+sub reparse {
+    my ($self, $format_string, $parse_string) = @_;
+
+    $parse_string ||= $format_string;
+    $self->strptime($self->strftime($format_string), $parse_string);
+}
+
+sub yesterday {
+    my ($self, ) = @_;
+
+    $self = $self->get_object;
+
+    $self->localtime(($self - ONE_DAY)->reparse('%Y-%m-%d'));
+}
+
+sub tomorrow {
+    my ($self, ) = @_;
+
+    $self = $self->get_object;
+
+    $self->localtime(($self + ONE_DAY)->reparse('%Y-%m-%d'));
+}
+
+sub truncate {
+}
 
 1;
 __END__
 
 =head1 NAME
 
-Time::Piece::Factory -
+Time::Piece::Factory - Factory module for Time::Piece
 
 =head1 SYNOPSIS
 
   use Time::Piece::Factory;
+
+  my $time = Time::Piece->yesterday;
+  my $two_days_ago = $time->yesterday;
 
 =head1 DESCRIPTION
 
